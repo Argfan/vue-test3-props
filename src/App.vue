b8ce4e82200c3444e15757570bf964cd93d47778
Argfan <argfan2@gmail.com>
2021-10-31 17:41:38 +0600

    31.10

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <ProductList/>
+    <button type="button" class="btn btn-primary" @click="getData">GET</button>
   </div>
 </template>
 
@@ -12,7 +13,26 @@ export default {
   name: 'App',
   components: {
     ProductList
-  }
+  },
+  data(){
+    return{
+      film: {}
+      
+    }    
+  },
+  methods: {
+    getData(){
+      fetch(
+        'https://api.themoviedb.org/3/movie/76341?api_key=95003f7d4b568b631f25e606a803207e'
+        )
+        .then(response => response.json())
+        .then(data => {          
+          this.film = data          
+          console.log(this.film);
+        })
+      
+    }
+  }  
 }
 </script>
 
