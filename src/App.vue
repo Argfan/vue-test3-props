c2e8bfc0d5ee1c93573759e7340d4edf778641e0
Argen Egeshov <argfan2@gmail.com>
2021-11-08 12:05:21 +0600

    08.11

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,34 @@
 <template>
   <div id="app">
-    <ProductList/>
+    <!-- <ProductList/> -->
     <button type="button" class="btn btn-primary" @click="getData">GET</button>
+    {{film.title}} <br>
+    <img :src="'https://image.tmdb.org/t/p/w500'+film.poster_path" alt="">
   </div>
 </template>
 
 <script>
 
-import ProductList from './components/ProductList.vue'
+// import ProductList from './components/ProductList.vue'
 
 export default {
   name: 'App',
   components: {
-    ProductList
+    // ProductList
   },
   data(){
     return{
-      film: {}
-      
+      apiKey: 'api_key=95003f7d4b568b631f25e606a803207e',
+      url1: 'https://api.themoviedb.org/3/movie/76341?',
+      url2: 'https://api.themoviedb.org/3/genre/movie/list?',
+      url3: '',
+      film: {}      
     }    
   },
   methods: {
     getData(){
       fetch(
-        'https://api.themoviedb.org/3/movie/76341?api_key=95003f7d4b568b631f25e606a803207e'
+        this.url2+this.apiKey
         )
         .then(response => response.json())
         .then(data => {          
